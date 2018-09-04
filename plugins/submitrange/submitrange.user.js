d6679b7f211e58dd9d24a1779ee7a26e43683d0e
Maarten Duwel <maartenduwel@hotmail.com>
2018-09-04 10:23:32 +0200

    added author

--- plugins/submitrange/submitrange.user.js
+++ plugins/submitrange/submitrange.user.js
@@ -3,6 +3,7 @@
 // @name           IITC plugin: Portal submitrange
 // @category       Highlighter
 // @version        1.0.0.20180409.010107
+// @author         Wintervorst
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Wintervorst/iitc/raw/master/plugins/submitrange/submitrange.user.js
 // @downloadURL    https://github.com/Wintervorst/iitc/
