6a46162ec7200bfc77334f509025c6cbfb333088
Maarten Duwel <>
2018-10-03 15:38:45 +0200

    Enabled edits

--- plugins/totalrecon/totalrecon.user.js
+++ plugins/totalrecon/totalrecon.user.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-totalrecon@wintervorst
 // @name           IITC plugin: Total Recon
 // @category       Highlighter
-// @version        0.0.7.20180210.013370
+// @version        0.1.0.20180310.013370
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Wintervorst/iitc/raw/master/plugins/totalrecon/totalrecon.user.js
 // @downloadURL    https://github.com/Wintervorst/iitc/raw/master/plugins/totalrecon/totalrecon.user.js
-// @description    [iitc-20180210.013370] Place markers on the map for possible candidates, submitted candidates, rejected candidates and succesful candidates.
+// @description    [iitc-20180310.013370] Place markers on the map for possible candidates, submitted candidates, rejected candidates and succesful candidates.
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
@@ -22,7 +22,6 @@ var L; // to prevent script errors on load
 var $; // to prevent script errors on load
 var map; // to prevent script errors on load
 
-
 // Level 6 cells are used to determine where to find the next candidate
 // TODO: Add counters and percentages to cells
 // TODO: Draw every next day in another color
@@ -35,33 +34,32 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
   plugin_info.buildName = 'iitc';
-  plugin_info.dateTimeVersion = '20180210.013370';
+  plugin_info.dateTimeVersion = '20180310.013370';
   plugin_info.pluginId = 'totalrecon';
   // PLUGIN START ///////////////////////////////////////////////////////
 
   // use own namespace for plugin
   window.plugin.totalrecon = function() {};
+  window.plugin.totalrecon.editmarker = {};    
   window.plugin.totalrecon.isPlacingMarkers = false;
-  window.plugin.totalrecon.initialized = false;
-  window.plugin.totalrecon.mouseMarker = null;
-  window.plugin.totalrecon.trackMarker = null;
-
+  window.plugin.totalrecon.initialized = false;    
+  window.plugin.totalrecon.markercollection = [];    
+  window.plugin.totalrecon.plottedmarkers = {};      
   window.plugin.totalrecon.scriptURL = localStorage.getItem('totalrecon.scriptURL');
-
   window.plugin.totalrecon.highlight = function(data) {}
-
+  
   window.plugin.totalrecon.setSelected = function(selected) {
     window.plugin.totalrecon.isPlacingMarkers = selected;
- }
+  }
 
   window.plugin.totalrecon.getStoredData = function() {
      $.ajax({
         url: window.plugin.totalrecon.scriptURL,
         type: 'GET',
         dataType: 'text',
-        success: function (data, status, header) {
-          console.log(data);
-            window.plugin.totalrecon.drawMarkers(JSON.parse(data));
+        success: function (data, status, header) {						
+            window.plugin.totalrecon.markercollection = JSON.parse(data);
+            window.plugin.totalrecon.drawMarkers();
         },
         error: function (x, y, z) {
             console.log('Error message: ' + x + '\n' + y + '\n' + z);
@@ -69,16 +67,17 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
     });
   }
 
-  window.plugin.totalrecon.drawMarkers = function (data) {
-		var markerColor = 'grey';
-		var markerLayer = window.plugin.totalrecon.candidateLayer;
-     if (data.length > 0) {
-       $.each(data, function(i, candidate) {
-         console.log(candidate);
-    	  if (candidate != undefined && candidate[3] != '' && candidate[4] != '') {
-			var portalLatLng = L.latLng(candidate[3], candidate[4]);
-			var status = candidate[5];
-            var title = candidate[1];
+  window.plugin.totalrecon.drawMarker = function(candidate) {
+    if (candidate != undefined && candidate.lat != '' && candidate.lng != '') {            
+      
+      var existingMarker = window.plugin.totalrecon.plottedmarkers[candidate.id];
+      if (existingMarker !== undefined) {
+       	 existingMarker.layer.removeLayer(existingMarker.marker);        
+      }
+      
+	  var portalLatLng = L.latLng(candidate.lat, candidate.lng);
+	  var status = candidate.status;
+      var title = candidate.title;
 			switch(status) {
 			   case 'candidate':
 				markerColor = 'grey';
@@ -99,9 +98,13 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 			}
 
 			var marker = createGenericMarker(portalLatLng, markerColor, {
-				title: title
+				title: title, 
+        id:candidate.id,
+        data:candidate
 			});
 			markerLayer.addLayer(marker);
+      
+      window.plugin.totalrecon.plottedmarkers[candidate.id] = {'marker':marker, 'layer':markerLayer};
 
             if (title != '') {
                 var titleMarker = L.marker(portalLatLng, {
@@ -114,42 +117,93 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
                 });
                 window.plugin.totalrecon.titleLayer.addLayer(titleMarker);
             }
-
-		}
+			}    
+  }
+  
+  window.plugin.totalrecon.drawMarkers = function () {
+		var markerColor = 'grey';
+		var markerLayer = window.plugin.totalrecon.candidateLayer;
+     if (window.plugin.totalrecon.markercollection.length > 0) {
+       $.each(window.plugin.totalrecon.markercollection, function(i, candidate) {         
+    			window.plugin.totalrecon.drawMarker(candidate);
   	   });
      }
   }
 
  window.plugin.totalrecon.onClick = function (e) {
      if (window.plugin.totalrecon.isPlacingMarkers) {
+       	if (window.plugin.totalrecon.editmarker != undefined) {
+			      window.map.removeLayer(window.plugin.totalrecon.editmarker);
+      	 };
          window.plugin.totalrecon.fireCreatedEvent(e.latlng);
      }
  };
 
   var form;
 
-   window.plugin.totalrecon.fireCreatedEvent = function (latlng) {
-		//var marker = new L.Marker(this._marker.getLatLng(), { icon: this.options.icon });
-    var marker = createGenericMarker(latlng, 'pink', {
-         title: 'Url location'
-     });
+   window.plugin.totalrecon.fireCreatedEvent = function (latlng) {	
+      var marker = createGenericMarker(latlng, 'pink', {
+         title: 'Place your mark!'
+     	});
+     
+      window.plugin.totalrecon.editmarker = marker;
 
-    marker.addTo(window.map);
-        var markerLatLng = latlng;
+    	marker.addTo(window.map);
+     
+  	  window.plugin.totalrecon.drawInputPopop(latlng);
+	};
+  
+  
+  window.plugin.totalrecon.drawInputPopop = function(latlng, markerData) {   	    
         var formpopup = L.popup();
-        formpopup.setLatLng(markerLatLng);
-         formpopup.setContent('<form id="submit-to-google" name="submit-to-google-sheet" style="width:200px;height:140px;">'
-                              + '<select name="status" style="clear:both; float:left; width:100%"><option value="candidate">Candidate</option><option value="submitted">Submitted</option><option value="live">Live</option><option value="rejected">Rejected</option></select>'
-                              + '<input name="title" style="clear:both; float:left; width:100%" type="text" placeholder="Titel" required>'
-                              + '<input name="description" style="clear:both; float:left; width:100%" type="text" placeholder="Description">'
-                              + '<input name="submitteddate" style="clear:both; float:left; width:100%" type="text" placeholder="Submitted (dd-mm-jjjj)">'
-                              + '<input name="responsedate" style="clear:both; float:left; width:100%" type="text" placeholder="Response (dd-mm-jjjj)">'
-                              + '<input name="lat" type="hidden" value="' + markerLatLng.lat +  '">'
-                              + '<input name="lng" type="hidden" value="' + markerLatLng.lng +  '">'
+        
+    		var title = '';
+    		var description = ''
+        var id = '';
+    		var submitteddate = '';
+    		var responsedate = '';
+    		var lat = '';
+        var lng = '';
+    		var status = 'candidate';
+    
+    		if (markerData !== undefined) {
+          	id = markerData.id;
+          	title = markerData.title;
+          	description = markerData.description;
+          	submitteddate = markerData.submitteddate;
+          	responsedate = markerData.responsedate;  
+            status = markerData.status;          	
+        } else {
+          	lat = latlng.lat;
+            lng = latlng.lng;
+        }
+    
+    		formpopup.setLatLng(latlng);
+    
+        formpopup.setContent('<form id="submit-to-google" name="submit-to-google-sheet" style="width:200px;height:180px;">'
+                              + '<select name="status" style="clear:both; float:left; width:100%">'
+                              +  '<option value="candidate"' + window.plugin.totalrecon.setselected('candidate',status)  +'>Candidate</option>'
+                             + '<option value="submitted"' + window.plugin.totalrecon.setselected('submitted',status)  +'>Submitted</option>'
+                             + '<option value="live"' + window.plugin.totalrecon.setselected('live',status)  +'>Live</option>'
+                             +  '<option value="rejected"' + window.plugin.totalrecon.setselected('rejected',status)  +'>Rejected</option></select>'
+                              + '<input name="title" style="clear:both; float:left; width:100%; height:30px;" type="text" placeholder="Title (required)" required value="' + title +  '">'
+                              + '<input name="description" style="clear:both; float:left; width:100%; height:30px;" type="text" placeholder="Description" value="' + description +  '">'
+                              + '<input name="submitteddate" style="clear:both; float:left; width:100%; height:30px;" type="text" placeholder="Submitted (dd-mm-jjjj)" value="' + submitteddate +  '">'
+                              + '<input name="responsedate" style="clear:both; float:left; width:100%; height:30px;" type="text" placeholder="Response (dd-mm-jjjj)" value="' + responsedate +  '">'
+                              + '<input name="id" type="hidden" value="' + id +  '">'
+                              + '<input name="lat" type="hidden" value="' + lat +  '">'
+                              + '<input name="lng" type="hidden" value="' + lng +  '">'
                               + '<input name="nickname" type="hidden" value="' + window.PLAYER.nickname + '">'
                               + '<button type="submit" style="clear:both; float:left; width:100%;height:30px;">Send</button></form>');
         formpopup.openOn(window.map);
-	};
+  }
+    
+    window.plugin.totalrecon.setselected = function(optionvalue, selectedvalue) {
+      if (optionvalue === selectedvalue) {
+       	return ' selected="selected" '; 
+      }            
+    }
+  
 
   window.plugin.totalrecon.scriptUrlIsSet = function() {
     window.plugin.totalrecon.scriptURL = localStorage.getItem('totalrecon.scriptURL');
@@ -177,34 +231,44 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
      return true;
   }
 
+  window.plugin.totalrecon.markerClicked = function(event) {
+   	 // bind data to edit form
+     if (window.plugin.totalrecon.editmarker != undefined) {
+			  window.map.removeLayer(window.plugin.totalrecon.editmarker);
+     };
+     window.plugin.totalrecon.drawInputPopop(event.layer.getLatLng(), event.layer.options.data);
+  }
+  
+  
   // Initialize the plugin
   var setup = function() {
      if (window.plugin.totalrecon.scriptUrlIsSet()) {
 
         window.addPortalHighlighter('Total Recon', window.plugin.totalrecon);
-        window.plugin.totalrecon.candidateLayer = new L.LayerGroup();
-        window.plugin.totalrecon.submittedLayer = new L.LayerGroup();
-        window.plugin.totalrecon.rejectedLayer = new L.LayerGroup();
-        window.plugin.totalrecon.acceptedLayer = new L.LayerGroup();
-        window.plugin.totalrecon.titleLayer = new L.LayerGroup();
+        window.plugin.totalrecon.candidateLayer = new L.featureGroup();
+        window.plugin.totalrecon.submittedLayer = new L.featureGroup();
+        window.plugin.totalrecon.rejectedLayer = new L.featureGroup();
+        window.plugin.totalrecon.acceptedLayer = new L.featureGroup();
+       
+        window.plugin.totalrecon.titleLayer = new L.LayerGroup();                              
 
         window.addLayerGroup('Total Recon - Candidates', window.plugin.totalrecon.candidateLayer, true);
         window.addLayerGroup('Total Recon - Submitted', window.plugin.totalrecon.submittedLayer, true);
         window.addLayerGroup('Total Recon - Rejected', window.plugin.totalrecon.rejectedLayer, true);
         window.addLayerGroup('Total Recon - Accepted', window.plugin.totalrecon.acceptedLayer, true);
-        window.addLayerGroup('Total Recon - Titles', window.plugin.totalrecon.titleLayer, true);
-
+        window.addLayerGroup('Total Recon - Titles', window.plugin.totalrecon.titleLayer, true);             
+       
         $("<style>")
             .prop("type", "text/css")
             .html(".plugin-totalrecon-name {\
-font-size: 12px;\
-font-weight: bold;\
-color: gold;\
-opacity: 0.7;\
-text-align: center;\
-text-shadow: -1px -1px #000, 1px -1px #000, -1px 1px #000, 1px 1px #000, 0 0 2px #000; \
-pointer-events: none;\
-}")
+		font-size: 12px;\
+			font-weight: bold;\
+			color: gold;\
+			opacity: 0.7;\
+			text-align: center;\
+		text-shadow: -1px -1px #000, 1px -1px #000, -1px 1px #000, 1px 1px #000, 0 0 2px #000; \
+		pointer-events: none;\
+				}")
             .appendTo("head");
 
 
@@ -212,13 +276,35 @@ pointer-events: none;\
             console.log(e);
             e.preventDefault();
             map.closePopup();
-            fetch(window.plugin.totalrecon.scriptURL, { method: 'POST', body: new FormData(e.currentTarget)})
-                .then(response => function() {
-                console.log('Success!', response);
-            }).catch(error => console.error('Error!', error.message))
+             $.ajax({
+        			url: window.plugin.totalrecon.scriptURL,
+        			type: 'POST',
+            	data: new FormData(e.currentTarget),
+              processData: false,
+        			contentType: false,
+        			success: function (data, status, header) {
+									window.plugin.totalrecon.drawMarker(data);
+            			if (window.plugin.totalrecon.editmarker != undefined) {
+			              window.map.removeLayer(window.plugin.totalrecon.editmarker);
+      					  };
+
+            
+        			},
+        		error: function (x, y, z) {
+            		console.log('Error message: ' + x + '\n' + y + '\n' + z);
+        		}
+    					});
+           
         });
 
         window.map.on('click', function(e) { window.plugin.totalrecon.onClick(e); });
+       
+       //window.map.addLayer(window.plugin.totalrecon.markerEditLayer);
+      
+   	    window.plugin.totalrecon.candidateLayer.on("click", window.plugin.totalrecon.markerClicked);       
+        window.plugin.totalrecon.submittedLayer.on("click", window.plugin.totalrecon.markerClicked);       
+        window.plugin.totalrecon.rejectedLayer.on("click", window.plugin.totalrecon.markerClicked);			   						       
+        window.plugin.totalrecon.acceptedLayer.on("click", window.plugin.totalrecon.markerClicked);			   					
 
         window.plugin.totalrecon.getStoredData();
         window.plugin.totalrecon.initialized = true;
