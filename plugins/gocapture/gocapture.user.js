8d6b6f634be3d0716f0cf6303c775199a068ecbd
Wintervorst <4071836+Wintervorst@users.noreply.github.com>
2021-05-26 10:30:03 +0200

    clearlayers after map reload

--- plugins/gocapture/gocapture.user.js
+++ plugins/gocapture/gocapture.user.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-go-capture
 // @name           IITC plugin: Go Capture!
 // @category       Info
-// @version        0.0.5.20210526.11236
+// @version        0.0.6.20210526.11236
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Wintervorst/iitc/raw/master/plugins/gocapture/gocapture.user.js
 // @downloadURL    https://github.com/Wintervorst/iitc/raw/master/plugins/gocapture/gocapture.user.js
-// @description    0.0.5 - Go Capture! - Highlights available unique captures. Captures are stored in the browser for more reliable results.
+// @description    0.0.6 - Go Capture! - Highlights available unique captures. Captures are stored in the browser for more reliable results.
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @include        http://intel.ingress.com/*
@@ -48,6 +48,7 @@ function wrapper (plugin_info) {
       return
 
     if (window.map.hasLayer(window.plugin.gocapture.uncapturedAvailableLayer)) {
+      window.plugin.gocapture.uncapturedAvailableLayer.clearLayers();
       $.each(window.portals, function (i, portal) {
         window.plugin.gocapture.drawAvailableUncaptured(portal)
       })
